c9a38e6f0595298e2d43198e6720b63b756cd20d
jasonlerit <jasonlerit077@gmail.com>
2023-04-07 12:16:26 +0800

    sample util

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,18 @@
 const Home = () => {
   return (
     <div>
-      <div>Home</div>
+      <div className='flex flex-col gap-4'>
+        <button className='bg-blue-500 px-4 py-2' onClick={() => {
+          import('../utils/sample_util').then(module => {
+            alert(module.add(1, 4))
+          })
+        }}>Add 1 + 4</button>
+        <button className='bg-blue-500 px-4 py-2' onClick={() => {
+          import('../utils/sample_util').then(module => {
+            alert(module.subtract(5, 1))
+          })
+        }}>Subtract 5 - 1</button>
+      </div>
     </div>
   )
 }
